54e60f2eebd2b80daf8b026fa338fb79ccee6bd3
Gonzalo Casas <casas@arch.ethz.ch>
2026-05-15 12:24:44 +0200

    Bump version to 1.0.0

--- src/compas_robots/__init__.py
+++ src/compas_robots/__init__.py
@@ -11,7 +11,7 @@ __author__ = ["Gonzalo Casas"]
 __copyright__ = "COMPAS Association"
 __license__ = "MIT License"
 __email__ = "casas@arch.ethz.ch"
-__version__ = "0.6.0"
+__version__ = "1.0.0"
 
 
 from .configuration import Configuration
